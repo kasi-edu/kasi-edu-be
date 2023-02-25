c9c43e18e819dcf20688359246a33e9538cdbd4f
endy <7ofpentacles@gmail.com>
2023-02-26 00:02:45 +0700

    husky

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
+import { dataSourceOptions } from '../db/data-source';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.env.${process.env.STAGE.toLowerCase()}`,
     }),
+    TypeOrmModule.forRoot(dataSourceOptions),
   ],
 })
 export class AppModule {}
